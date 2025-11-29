f2cd90063261001665d77f8f75418f47ddd6651d
whcorkran <hcorkran@seas.upenn.edu>
2025-11-28 19:46:27 -0500

    using new mason feature

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -302,18 +302,18 @@ return {
         'cpplint',
         'clang-format',
       })
+
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
       require('mason-lspconfig').setup {
-        ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
+        ensure_installed = {},
         automatic_installation = false,
         handlers = {
           function(server_name)
-            print('Handled', server_name)
+            vim.notify('HANDLER: ' .. server_name, vim.log.levels.INFO) -- This shows a popup
+            print('HANDLER CALLED FOR:', server_name) -- This goes to :messages
+
             local server = servers[server_name] or {}
-            -- This handles overriding only values explicitly passed
-            -- by the server configuration above. Useful when disabling
-            -- certain features of an LSP (for example, turning off formatting for ts_ls)
             server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
             require('lspconfig')[server_name].setup(server)
           end,
